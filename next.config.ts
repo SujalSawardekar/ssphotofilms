3a8265f472b98b4723d39abc0bd4e4791fa5c6bf
SujalSawardekar <133184441+SujalSawardekar@users.noreply.github.com>
2026-04-08 23:40:34 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+const nextConfig = {
+  // Bypasses ESLint errors during the build
   eslint: {
     ignoreDuringBuilds: true,
   },
